68d13cffc47990df314a453f25f7b8daf228ae9f
Franck <franck.o.perrin@free.fr>
2012-02-24 18:43:13 +0000

    Home page update Changed username

--- index.php
+++ index.php
@@ -44,9 +44,47 @@
                     <li><a href="Blog">Blog</a></li>
                     <li><a href="Wiki">Wiki</a></li>
                 </ul>
+				<div class="clearfix"></div>
 			</header>
-			<div role="main">
-
+			<div id="main" role="main">
+				<div id="intro">
+						<h1>WIDGaT &ndash; Widget Design Authoring Toolkit</h1>						
+						<p>
+							&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b id="ext-gen1191">W</b>idget <b>D</b>esign <b>A</b>uthoring <b>T</b>oolkit <b>(WIDGaT)</b> is a JISC-funded Learning and Teaching Innovation project led by the Accessibility Research Centre at Teesside University.The WIDGaT toolkit supports the design, development and sharing of widgets, by enabling teachers and students to easily create bespoke widgets.
+						</p>
+				</div>
+				<div id="twitterWidget">
+					<script charset="utf-8" src="http://widgets.twimg.com/j/2/widget.js"></script>
+					<script>
+						new TWTR.Widget({
+							version: 2,
+							type: 'profile',
+							rpp: 5,
+							interval: 30000,
+							width: 220,
+							height: 200,
+							theme: {
+								shell: {
+									background: '#52729c',
+									color: '#ffffff'
+								},
+								tweets: {
+									background: '#ffffff',
+									color: '#000000',
+									links: '#52729c'
+								}
+							},
+							features: {
+								scrollbar: true,
+								loop: true,
+								live: true,
+								behavior: 'all'
+							}
+						}).render().setUser('ARC_Teesside').start();
+					</script>
+				</div>
+				
+				<div class="clearfix"></div>
 			</div>
 			<footer>
 
