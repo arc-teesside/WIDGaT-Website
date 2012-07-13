610aa69e4ceaed57fc1172ad860ec821d487e5fd
Franck <franck.o.perrin@free.fr>
2012-07-13 16:11:23 +0100

    updated themes and plugins

--- blog/wp-content/plugins/atbar/atbar.php
+++ blog/wp-content/plugins/atbar/atbar.php
@@ -3,70 +3,29 @@
 Plugin Name: ATbar
 Plugin URI: http://www.atbar.org
 Description: ATbar is a cross browser accessibility toolbar which can be added to a user's browser or alternatively, for developers, the toolbar launcher can be embedded on a website.
-Version: 1.0
-Author: Magnus White
+Version: 3.1
+Author: Magnus P White
 Author Email: mpw@ecs.soton.ac.uk
 Author URI: http://access.ecs.soton.ac.uk
 */
-
 
 // registure plugin with Wordpress
 add_action('admin_menu', 'atbar_register_settings');
 add_action('admin_menu', 'atbar_add_options');
 
-function atbar_register_settings()
-{
-	register_setting('atbar_options', 'atbar_persistent');
-}
-
-function atbar_add_options()
-{
-	add_options_page('Atbar', 'Atbar', 'manage_options', 'atbaroptions', 'atbar_options');
-}
-
-
-// adds css to header
-add_action('wp_head', 'atbar');
-
-function atbar() {
-	echo ('<link rel="stylesheet" type="text/css" media="all" href="'.plugin_dir_url(__FILE__).'atbar.css">');
-}
-
-
+// adds functions & widget class
+include_once ('functions/functions.php');
+include_once ('functions/atbarwidget.class.php');
 
 // add options menu
 include(dirname(__FILE__).'/options.php');
 
-$persistent_option = get_option('atbar_persistent');
+// adds css to header
+add_action('wp_print_styles', 'atbar_css');
 
-if($persistent_option == "true") {
+// adds the toolbar itself
+add_action('the_post', 'add_toolbar');
 
-	add_action('wp_head', 'persistentlaunch');
-
-	function persistentlaunch() {
-
-		$persistenttoolbar = file_get_contents(dirname(__FILE__).'/atbar-launcher.js');
-		
-		echo ('<script language="javascript">'.$persistenttoolbar.'</script>');
-	}
-}
-else {
-	// adds toolbar launcher
-	add_action('wp_head', 'toolbarlauncher');
-
-	function toolbarlauncher() {
-
-		$toolbar = addslashes(file_get_contents(dirname(__FILE__).'/atbar-launcher.php'));
-		echo ('<script language="javascript">
-		
-		jQuery(document).ready( function(){
-					jQuery("body").prepend("' . $toolbar . '");
-				}
-
-				);	
-		</script>');
-	}
-}
-
+// adds ATbar widget
+add_action( 'widgets_init', create_function( '', 'register_widget( "atbar_widget" );' ) );
 ?>
-
--- blog/wp-content/plugins/atbar/options.php
+++ blog/wp-content/plugins/atbar/options.php
@@ -3,32 +3,103 @@
 function atbar_options()
 {
 	
+	$version_option = get_option('atbar_version');
+	$marketplace_toolbar_option = get_option('atbar_marketplace_toolbar');
 	$persistent_option = get_option('atbar_persistent');
+	$exclude_option = get_option('atbar_exclude');
+	$exclude_pages_option = get_option('atbar_exclude_pages');
+	$exclude_option = get_option('atbar_launcher_exclude');
+	$exclude_launcher_pages_option = get_option('atbar_launcher_exclude_pages');
+	$atbar_launcher_image_option = get_option('atbar_launcher_image');
 	
-	// display options page
-	echo ('<div class="wrap">
+?>
+	
+	<div class="wrap">
 	<div id="icon-options-general" class="icon32"><br /></div>
-	<h2>Atbar Options</h2>
-
-	<form method="post" action="options.php">');
-	settings_fields('atbar_options');
+	<h2>Atbar Options</h2>	
 	
-	echo ('
-	<table class="form-table">
-		<tr valign="top">
-			<th scope="row">Is ATbar persistent on all pages:</th>
-			<td><b>'.$persistent_option.'</b></td>
-		</tr>
-		<tr valign="top">
-			<th scope="row">Load ATbar on all pages?</th>
+	<form method="post" action="options.php">
+	<? settings_fields("atbar_options"); ?>
+	
+	<p><b>For instructions on this plugin view the <a href="http://www.atbar.org/wordpress-plugin-guide">ATbar Wordpress Plugin Guide</a>.</b></p>
+	
+	<p>The ATbar sidebar widget can be added in Appearance, Widgets then adding ATbar to your active Widgets.</p>
+	
+	
+	<table class="form-table" style="border-top: 1px solid #DFDFDF;">
+	
+		<!-- select version to use -->		
+		<tr>
+			<th scope="row">Select version:</th>
 			<td>
-				<select name="atbar_persistent" width="60px">
-					<option value="false">No</option>
-					<option value="true">Yes</option>
+				<select name="atbar_version">
+					<option value="en"	<? echo is_version("en"); ?>>English</option>
+					<option value="ar" <? echo is_version("ar"); ?>>Arabic</option>
+					<option value="marketplace"	<? echo is_version("marketplace");	?>>Toolbar from ATbar Marketplace</option>
+				</select>
+				
+				<p>Create your own toolbar at <a href="http://marketplace.atbar.org">http://marketplace.atbar.org</a> or use one of the pre-made toolbars.</p>
+			</td>
+		</tr>
+			<th>Toolbar from ATbar Marketplace:</th>
+			<td>
+				<input type="text" name="atbar_marketplace_toolbar" value="<? echo $marketplace_toolbar_option ?>" style="width:350px;">
+				<p>Copy the install button link in the text box (the full javascript string)</p>
+				<p>For further instructions view the <a href="http://www.atbar.org/wordpress-plugin-guide">guide</a>.</p>
+			</td>
+		</tr>
+		
+		<!-- load atbar launcher -->		
+		<tr style="border-top: 1px solid #DFDFDF;">
+			<th scope="row">Load ATbar launcher at the top of the page? (select <b>no</b> for when using the ATbar widget)</th>
+			<td>
+				<select name="atbar_launcher_image">
+						<option value="No" <? echo is_banner_show("No"); ?>>No</option>
+						<option value="Yes" <? echo is_banner_show("Yes");	?>>Yes</option>
 				</select>
 			</td>
 		</tr>
 		
+		<!-- exclude pages for laucher -->
+		<tr>
+			<th scope="row">Exclude pages for ATbar launcher?</th>
+			<td><input type="checkbox" name="atbar_launcher_exclude" lable="exclude pages for launcher" <? is_exclude('atbar_launcher_exclude'); ?>>  Exclude pages for ATbar launcher</td>
+		</tr>
+				
+		<tr>
+			<td></td>
+			<td>
+				<input type="text" name="atbar_launcher_exclude_pages" value="<? echo $exclude_launcher_pages_option; ?>" style="width:346px;">
+				<p>Enter a comma-separated list of page IDs or name to exclude, e.g. 1,2,3 etc.</p>
+			</td>
+		</tr>
+		
+		<!-- auto load atbar -->		
+		<tr style="border-top: 1px solid #DFDFDF;">
+			<th scope="row">Auto load ATbar on all pages? (user doesn't have to select to load each time)</th>
+			<td>
+				<select name="atbar_persistent">
+					<option value="No" <? echo is_persistent("No"); ?>>No</option>
+					<option value="Yes" <? echo is_persistent("Yes"); ?>>Yes</option>
+				</select>
+			</td>
+		</tr>
+		
+		<!-- exclude pages for auto loader -->
+		<tr>
+			<th scope="row">Exclude pages for auto load?</th>
+			<td><input type="checkbox" name="atbar_exclude" lable="exclude pages for auto loader" <? is_exclude('atbar_exclude'); ?>>  Exclude pages for auto load</td>
+		</tr>
+				
+		<tr>
+			<td></td>
+			<td>
+				<input type="text" name="atbar_exclude_pages" value="<? echo $exclude_pages_option; ?>" style="width:346px;">
+				<p>Enter a comma-separated list of page IDs or name to exclude, e.g. 1,2,3 etc.</p>
+			</td>
+		</tr>
+		
+		<!-- save form -->		
 		<tr valign="top">
 			<th scope="row">
 				<p class="submit">
@@ -38,15 +109,14 @@ function atbar_options()
 		</tr>
 		</form>
 		
-		<br />
-		
-		<form method="post" action="options.php">');
-		settings_fields('atbar_options');
-		echo('
-		<input type="hidden" name="atbar_persistent" value="'.get_option('atbar_persistent').'" />
+		<form method="post" action="options.php">
+			<? settings_fields('atbar_options'); ?>
+			<input type="hidden" name="atbar_persistent" value="<? get_option('atbar_persistent') ?>" />
 		</form>	
-	</table>');
+
+	</table>
+<?
+	
 }
 
 ?>
-
